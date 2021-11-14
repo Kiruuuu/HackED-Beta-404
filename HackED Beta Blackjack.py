2da45b124c25171de49178341eefed3188e5c7e1
Kiruuuu <thomasyoo11@gmail.com>
2021-11-14 06:14:31 -0700

    finally debugged a bit of code because i forgot "10" is longer than 1 character...... .............................. player bust should be more or less fully implemented (aces might be buggy and game breaking and overpowered)

--- HackED Beta Blackjack.py
+++ HackED Beta Blackjack.py
@@ -86,11 +86,13 @@ def card_img(dealt_cards, img_call_list):
 		img_call_list[card] = pg.image.load(img_call_list[card])
 	for value in range(len(card_val)):
 		card_val[value] = str(card_val[value])
+		if card_val[value] == '1':
+			card_val[value] = '10'
 		if card_val[value] in royalty:
 			card_val[value] = '10'
 		if card_val[value].isalpha() is False:
 			card_val[value] = int(card_val[value])
-	return img_call_list
+	return img_call_list, card_val
 
 
 def play():
@@ -100,7 +102,7 @@ def play():
 	window = pg.display.set_mode((screen_width, screen_height))
 	pg.display.set_caption("HackED Beta Blackjack 404")
 
-	img_call_list = card_img(card_deal(), setup())
+	img_call_list, card_val = card_img(card_deal(), setup())
 	hit_button = button((0,255,0), 0, 475, 225, 100,'HIT')
 	stand_button = button((0,255,0), 225, 475, 225, 100,'STAND')
 	quit_button = button((0,255,0), 975, 475, 225, 100, 'QUIT')
@@ -111,76 +113,116 @@ def play():
 	stand = False
 	hit_num = 0
 	bust = False
+	score = 0
+	if card_val[0] == card_val[1] == 'a':
+		score = 12
+	elif card_val[0] == 'a':
+		score = 11 + card_val[1]
+	elif card_val[1] == 'a':
+		score = 11 + card_val[0]
+	else:
+		score = card_val[0] + card_val[1]
+	print(score)
 	while finish == False:
-			window.fill(bg)
-			hit_button.draw(window, (0,0,0))
-			stand_button.draw(window, (0,0,0))
-			quit_button.draw(window, (0,0,0))
-			window.blit(img_call_list[0], (0,575))
-			window.blit(img_call_list[1], (225,575))
-			window.blit(pg.image.load("cards/back.png"), (975,0))
-			window.blit(img_call_list[3], (750,0))
-			for event in pg.event.get():
-				if not bust:
-					pos = pg.mouse.get_pos()
-					if event.type == QUIT:
-						finish = True
-					if event.type == pg.MOUSEBUTTONDOWN:
-						if hit_button.over(pos) and (hit_num != 3) and (not stand):
-							hit_list[hit_num] = True
-							hit_num += 1
-						elif stand_button.over(pos):
-							stand = True
-							window.blit(img_call_list[2], (975,0))
-						elif quit_button.over(pos):
-							bust = True
-						elif (hit_num == 3) or stand:
-							hit_button.color = (128,128,128)
 
-					if event.type == pg.MOUSEMOTION:
-						if hit_button.over(pos) and (stand is False) and (hit_num != 3):
-							hit_button.color = (255,0,0)
-						elif (hit_num == 3) or stand:
-							hit_button.color = (128,128,128)
+		window.fill(bg)
+		hit_button.draw(window, (0,0,0))
+		stand_button.draw(window, (0,0,0))
+		quit_button.draw(window, (0,0,0))
+		window.blit(img_call_list[0], (0,575))
+		window.blit(img_call_list[1], (225,575))
+		window.blit(pg.image.load("cards/back.png"), (975,0))
+		window.blit(img_call_list[3], (750,0))
+
+		for event in pg.event.get():
+			if bust is False:
+				pos = pg.mouse.get_pos()
+				if event.type == QUIT:
+					finish = True
+				if event.type == pg.MOUSEBUTTONDOWN:
+					if hit_button.over(pos) and (hit_num != 3) and (not stand):
+						hit_list[hit_num] = True
+						if card_val[4+hit_num] == 'a':
+							score += 11
+							if score > 21:
+								score -= 10
+								if score > 21:
+									bust = True
 						else:
-							hit_button.color = (0,255,0)
-
-						if stand_button.over(pos) and (not stand):
-							stand_button.color = (255,0,0)
-						elif stand:
-							stand_button.color = (128,128,128)
-						else:
-							stand_button.color = (0,255,0)
-
-						if quit_button.over(pos):
-							quit_button.color = (255,0,0)
-				if bust:
-					pos = pg.mouse.get_pos()
-					if event.type == QUIT:
-						finish = True
-					bust_button.draw(window, (0,0,0))
-					if event.type == pg.MOUSEBUTTONDOWN:
-						if quit_button.over(pos):
+							score += card_val[4 + hit_num]
+						if (card_val[0] or card_val[1]) == 'a':
+							if score > 21:
+								score -= 10
+						if score > 21:
 							bust = True
-						if bust_button.over(pos):
-							play()
-					if event.type == pg.MOUSEMOTION:
-						if quit_button.over(pos):
-							quit_button.color = (255,0,0)
-						if bust_button.over(pos):
-							bust_button.color = (255,0,0)
+						print(score)
+						hit_num += 1
+					elif stand_button.over(pos):
+						stand = True
+						window.blit(img_call_list[2], (975,0))
+					elif quit_button.over(pos):
+						finish = True
+					elif (hit_num == 3) or stand:
+						hit_button.color = (128,128,128)
 
+				if event.type == pg.MOUSEMOTION:
+					if hit_button.over(pos) and (stand is False) and (hit_num != 3):
+						hit_button.color = (255,0,0)
+					elif (hit_num == 3) or stand:
+						hit_button.color = (128,128,128)
+					else:
+						hit_button.color = (0,255,0)
+
+					if stand_button.over(pos) and (not stand):
+						stand_button.color = (255,0,0)
+					elif stand:
+						stand_button.color = (128,128,128)
+					else:
+						stand_button.color = (0,255,0)
+
+					if quit_button.over(pos):
+						quit_button.color = (255,0,0)
+					else:
+						quit_button.color = (0,255,0)
+			if bust is True:
+				hit_button.color = (128,128,128)
+				stand_button.color = (128,128,128)
+				pos = pg.mouse.get_pos()
+				bust_button.draw(window, (0,0,0))
+				if event.type == QUIT:
+					finish = True
+
+				if event.type == pg.MOUSEBUTTONDOWN:
+					if quit_button.over(pos):
+						finish = True
+					if bust_button.over(pos):
+						play()
+
+				if event.type == pg.MOUSEMOTION:
+					if quit_button.over(pos):
+						quit_button.color = (255,0,0)
+					else: 
+						quit_button.color = (0,255,0)
+					if bust_button.over(pos):
+						bust_button.color = (255,0,0)
+					else:
+						bust_button.color = (0,255,0)
 			for i in range(len(hit_list)):
 				if hit_list[i]:
 					window.blit(img_call_list[4 + i], (450 + 225*i,575))
-
 			if stand:
 				window.blit(img_call_list[2], (975,0))
+			
 			pg.display.update()
 			timer.tick(fps)
+
+
 	pg.display.quit()
 	pg.quit()
 
 
 if __name__ == "__main__":
 	play()
+
+
+
