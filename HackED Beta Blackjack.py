f54d916286e57f9264d54ef07044f37981106054
Kiruuuu <thomasyoo11@gmail.com>
2021-11-14 05:09:39 -0700

    hella functionality implemented can now restart game if bust (idea of bust not yet implemented though oops) no need to relaunch program in order to play again need to fix some flickering of bust gui window and colour of bust button all important non-one-time things in functions

--- HackED Beta Blackjack.py
+++ HackED Beta Blackjack.py
@@ -14,7 +14,6 @@ bg = pg.Color(0, 120, 0)
 suits = ["club", "dia", "heart", "spade"]
 royalty = ["j", "q", "k"]
 cards = []
-img_call_list = []
 card_val = []
 
 for suit in suits:
@@ -26,6 +25,7 @@ for suit in suits:
 	for royal in royalty:
 		card = str(royal) + "_" + suit
 		cards.append(card)
+
 
 class button():
 	def __init__(self, color, x,y,width,height, text=''):
@@ -44,7 +44,7 @@ class button():
 		pg.draw.rect(win, self.color, (self.x,self.y,self.width,self.height),0)
 
 		if self.text != '':
-			font = pg.font.SysFont('comicsans', 60)
+			font = pg.font.SysFont('cambria', 60)
 			text = font.render(self.text, 1, (0,0,0))
 			win.blit(text, (self.x + (self.width/2 - text.get_width()/2), self.y + (self.height/2 - text.get_height()/2)))
 
@@ -56,8 +56,13 @@ class button():
 
 		return False
 
+def setup():
+	img_call_list = []
+	return img_call_list
+
+
 def card_deal():
-	dealt_cards = [None] * 14
+	dealt_cards = [None] * 10
 	loop_cond = True
 	while loop_cond:
 		for card in range(len(dealt_cards)):
@@ -71,70 +76,111 @@ def card_deal():
 	return dealt_cards
 
 
-def card_img():
-	dealt_cards = card_deal()
-	print(dealt_cards)
+def card_img(dealt_cards, img_call_list):
+	
 	# Changing from card index values to actual cards
-
-
 	for card in range(len(dealt_cards)):
 		dealt_cards[card] = cards[dealt_cards[card]]
 		card_val.append(dealt_cards[card][0])
 		img_call_list.append("cards/" + dealt_cards[card] + ".png")
 		img_call_list[card] = pg.image.load(img_call_list[card])
 	for value in range(len(card_val)):
+		card_val[value] = str(card_val[value])
 		if card_val[value] in royalty:
 			card_val[value] = '10'
 		if card_val[value].isalpha() is False:
 			card_val[value] = int(card_val[value])
-
-	print(card_val)
-	print(dealt_cards)
-	print(img_call_list)
-
 	return img_call_list
 
-img_call_list = card_img()
 
-# PyGame initialization
-pg.init()
-timer = pg.time.Clock()
-window = pg.display.set_mode((screen_width, screen_height))
-pg.display.set_caption("HackED Beta Blackjack 404")
+def play():
+	# PyGame initialization
+	pg.init()
+	timer = pg.time.Clock()
+	window = pg.display.set_mode((screen_width, screen_height))
+	pg.display.set_caption("HackED Beta Blackjack 404")
 
-hit_button = button((0,255,0), 0, 475, 225, 100,'HIT')
-stand_button = button((0,255,0), 225, 475, 225, 100,'STAND')
-# Game Loop
-finish = False
-while finish == False:
-	window.fill(bg)
-	hit_button.draw(window, (0,0,0))
-	stand_button.draw(window, (0,0,0))
-	window.blit(img_call_list[0], (0,575))
-	window.blit(img_call_list[1], (225,575))
-	window.blit(img_call_list[2], (975,0))
-	window.blit(img_call_list[3], (750,0))
+	img_call_list = card_img(card_deal(), setup())
+	hit_button = button((0,255,0), 0, 475, 225, 100,'HIT')
+	stand_button = button((0,255,0), 225, 475, 225, 100,'STAND')
+	quit_button = button((0,255,0), 975, 475, 225, 100, 'QUIT')
+	bust_button = button((0,255,0), 400, 350, 400, 100, 'BUST! RETRY?')
+	# Game Loop
+	hit_list = [False] * 3 # haha hitlist
+	finish = False
+	stand = False
+	hit_num = 0
+	bust = False
+	while finish == False:
+			window.fill(bg)
+			hit_button.draw(window, (0,0,0))
+			stand_button.draw(window, (0,0,0))
+			quit_button.draw(window, (0,0,0))
+			window.blit(img_call_list[0], (0,575))
+			window.blit(img_call_list[1], (225,575))
+			window.blit(pg.image.load("cards/back.png"), (975,0))
+			window.blit(img_call_list[3], (750,0))
+			for event in pg.event.get():
+				if not bust:
+					pos = pg.mouse.get_pos()
+					if event.type == QUIT:
+						finish = True
+					if event.type == pg.MOUSEBUTTONDOWN:
+						if hit_button.over(pos) and (hit_num != 3) and (not stand):
+							hit_list[hit_num] = True
+							hit_num += 1
+						elif stand_button.over(pos):
+							stand = True
+							window.blit(img_call_list[2], (975,0))
+						elif quit_button.over(pos):
+							bust = True
+						elif (hit_num == 3) or stand:
+							hit_button.color = (128,128,128)
 
-	for event in pg.event.get():
-		pos = pg.mouse.get_pos()
-		if event.type == QUIT:
-			finish = True
-		if event.type == pg.MOUSEBUTTONDOWN:
-			if hit_button.over(pos):
-				continue # give new card
-			elif stand_button.over(pos):
-				pass # let dealer play
-		if event.type == pg.MOUSEMOTION:
-			if hit_button.over(pos):
-				hit_button.color = (255,0,0)
-			else:
-				hit_button.color = (0,255,0)
-			if stand_button.over(pos):
-				stand_button.color = (255,0,0)
-			else:
-				stand_button.color = (0,255,0)
+					if event.type == pg.MOUSEMOTION:
+						if hit_button.over(pos) and (stand is False) and (hit_num != 3):
+							hit_button.color = (255,0,0)
+						elif (hit_num == 3) or stand:
+							hit_button.color = (128,128,128)
+						else:
+							hit_button.color = (0,255,0)
+
+						if stand_button.over(pos) and (not stand):
+							stand_button.color = (255,0,0)
+						elif stand:
+							stand_button.color = (128,128,128)
+						else:
+							stand_button.color = (0,255,0)
+
+						if quit_button.over(pos):
+							quit_button.color = (255,0,0)
+				if bust:
+					pos = pg.mouse.get_pos()
+					if event.type == QUIT:
+						finish = True
+					bust_button.draw(window, (0,0,0))
+					if event.type == pg.MOUSEBUTTONDOWN:
+						if quit_button.over(pos):
+							bust = True
+						if bust_button.over(pos):
+							play()
+					if event.type == pg.MOUSEMOTION:
+						if quit_button.over(pos):
+							quit_button.color = (255,0,0)
+						if bust_button.over(pos):
+							bust_button.color = (255,0,0)
+
+			for i in range(len(hit_list)):
+				if hit_list[i]:
+					window.blit(img_call_list[4 + i], (450 + 225*i,575))
+
+			if stand:
+				window.blit(img_call_list[2], (975,0))
+			pg.display.update()
+			timer.tick(fps)
+	pg.display.quit()
+	pg.quit()
 
 
-	pg.display.update()
-	timer.tick(fps)
-    
+if __name__ == "__main__":
+	play()
